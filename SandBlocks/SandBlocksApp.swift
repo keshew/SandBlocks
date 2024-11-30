79abfd17f0adb36c1a773ddd9c6a258d2878f2f5
keshew <artyom.korotckov2017@yandex.ru>
2024-11-30 13:23:04 +0500

    Initial Commit

---
+++ SandBlocks/SandBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandBlocksApp.swift
+//  SandBlocks
+//
+//  Created by Артём Коротков on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SandBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
